73a5951444bfcff47d401d420ed190ecb26d0cc3
Kevin Kabatra <kevinkabatra@gmail.com>
2017-05-08 19:54:38 -0400

    Addressed checksum issue, and minor additions to .nuspec file.
    1. Added bugTrackerUrl and releaseNotes sections to .nuspec file. 
    2. Updated checksum to sha512, fixed issue: https://github.com/kevinkabatra/mssyncframework21-corecomponents-x64/issues/1
    3.  Update chocolatey package to refelect above changes

--- package/tools/chocolateyinstall.ps1
+++ package/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿Install-ChocolateyPackage `
 	'MSSyncFramework21-sdk-x64' 'msi' "/quiet /norestart" `
 	'https://download.microsoft.com/download/E/1/E/E1E636E6-AA5C-4052-9927-D722312DB564/Synchronization-v2.1-x64-ENU.msi' `
-    -checksum '488B37247A809318BC86F7F94780CFEE' `
-    -checksumtype 'sha256'
+    -checksum 'EF3CDB48B903B81DA8061F64BBDE2ED87D4C4FA252F643F64A9678A6F109D1036E023C975F9FD76C619E1181C5A02DE2D28462286131DC17C9487D1217D75ADC' `
+    -checksumtype 'sha512'
