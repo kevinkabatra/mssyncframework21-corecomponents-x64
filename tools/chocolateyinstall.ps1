046be932cffba2d40bdfc4181fda29020cd9493f
Kevin Kabatra <kevinkabatra@gmail.com>
2017-05-08 15:46:06 -0400

    Initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿Install-ChocolateyPackage `
+	'MSSyncFramework21-sdk-x64' 'msi' "/quiet /norestart" `
+	'https://download.microsoft.com/download/E/1/E/E1E636E6-AA5C-4052-9927-D722312DB564/Synchronization-v2.1-x64-ENU.msi'
